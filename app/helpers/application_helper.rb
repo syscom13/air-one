f9cf1aa7fda868e7e6e1663d6aed6a9d5ec31ae3
Nicolas Verger <nicolas.india@gmail.com>
2017-11-18 00:55:32 +0100

    Add gravatar and avatar url + gravatar checker helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
+require 'net/http'
+
 module ApplicationHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=150"
+  end
+
+  def gravatar?(user)
+    gravatar_check = "http://gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email).downcase}.png?d=404"
+    uri = URI.parse(gravatar_check)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    response.code.to_i != 404 # from d=404 parameter
+  end
 end
+
