7e81e16609e3293f296236ec606738a327c01ebf
Nicolas Verger <nicolas.india@gmail.com>
2017-11-18 04:43:17 +0100

    Modify mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,11 +90,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
       address: 'smtp.sendgrid.net',
       port: 587,
-      domain: 'air-one.herokuapp.com',
+      domain: 'heroku.com',
       authentication: 'plain',
       user_name: ENV["SENDGRID_USERNAME"],
       password: ENV["SENDGRID_PASSWORD"],
