031c23d3af0b579ad1cf38aef0b9400f0ba15778
Nicolas Verger <nicolas.india@gmail.com>
2017-11-18 01:51:43 +0100

    Add initial config for mailer with Sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -53,4 +53,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      domain: 'localhost:3000',
+      authentication: 'plain',
+      user_name: ENV["sendgrid_username"],
+      password: ENV["sendgrid_password"],
+      enable_starttls_auto: true
+  }
 end
