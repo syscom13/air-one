2ecfcb40eeb46ae4119f9d8229e1ebfe19790ca9
Nicolas Verger <nicolas.india@gmail.com>
2017-11-24 20:02:45 +0100

    Config mailer for air-two prod on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'air-one.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'air-two.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       address: 'smtp.sendgrid.net',
       port: 587,
