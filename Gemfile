74d54a2522e519ea3e28b58436a254730f1538d0
Nicolas Verger <nicolas.india@gmail.com>
2017-11-29 11:31:07 +0100

    Add verification with SMS by implementing Twilio

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem 'geocoder', '~> 1.4'
 gem 'jquery-ui-rails', '~> 5.0'
 gem 'ransack', '~> 1.7'
 
+#----  Air Two  -------
+gem 'twilio-ruby', '~> 4.11.1'
+
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,24 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :guest_reviews, class_name: "GuestReview", foreign_key: "guest_id"
   has_many :host_reviews, class_name: "HostReview", foreign_key: "host_id"
+
+  def generate_pin
+    self.pin = SecureRandom.hex(2)
+    self.phone_verified = false
+    save
+  end
+
+  def send_pin
+    @client = Twilio::REST::Client.new
+    @client.messages.create(
+      from: ENV["phone_number"],
+      to: self.phone_number,
+      body: "Your pin is #{self.pin}"
+    )
+  end
+
+  def verify_pin(entered_pin)
+    update(phone_verified: true) if self.pin == entered_pin
+  end
+
 end
