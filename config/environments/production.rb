aa4cb1d6a867d1ea5497e6cab7d18d29746c7ed7
Nicolas Verger <nicolas.india@gmail.com>
2017-12-06 22:37:55 +0100

    Configure wss for action cable in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.url = "ws://air-two.herokuapp.com/cable"
+  config.action_cable.url = "wss://air-two.herokuapp.com/cable"
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'air-two.herokuapp.com' }
